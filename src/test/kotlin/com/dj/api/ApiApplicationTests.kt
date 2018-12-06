3d196b36b059711e6d22bb9762cb3a604f4d1c1f
Damian Jarosz <damianeusz@gmail.com>
2018-12-06 18:50:51 +0100

    Todos

--- src/test/kotlin/com/dj/api/ApiApplicationTests.kt
+++ src/test/kotlin/com/dj/api/ApiApplicationTests.kt
@@ -8,7 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.hamcrest.CoreMatchers.`is`
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -26,7 +27,7 @@ class ApiApplicationTests {
     fun whenGetPreorders_thenResponse200AndJson() {
         mvc.perform(
                 get("/api/v2/preorders"))
-                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(status().isOk)
     }
 
 
---
+++ todo.md
@@ -0,0 +1,15 @@
+Do it:
+1. tests skeleton
+
+Requirements:
+1. hal
+2. resiliency
+3. api docs
+4. ui
+5. docker
+6. k8s
+7. code design
+7. code quality
+8. tests quality
+9. performance
+10. readme
